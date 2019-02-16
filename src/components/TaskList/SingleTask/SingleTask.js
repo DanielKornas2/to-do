f450c77e135d2267512e60932221b93c73bc0aac
Daniel Kornas <harvi_pl@wp.pl>
2019-02-16 19:00:09 +0100

    Todo and completed task, conditional rendering

--- src/components/TaskList/SingleTask/SingleTask.js
+++ src/components/TaskList/SingleTask/SingleTask.js
@@ -1,16 +1,31 @@
 import React from 'react';
 
 const SingleTask = (props) => {
-    const { text, date, id } = props.task;
-    return ( 
-        <div>
-            <p>
-                <strong>{text}</strong> - till <span>{date} </span>
-                <button onClick={() => props.changeTaskStatus(id)} >Done</button>
-                <button onClick={() => props.deleteTask(id)}>X</button>
-            </p>    
-        </div>
-     );
+    const { text, date, id, active, finishDate, important } = props.task;
+      
+    if (active){
+        return ( 
+            <div>
+                <p>
+                    <strong><span className={important ? "important" : null}>{text}</span></strong> - till <span>{date} </span>
+                    <button onClick={() => props.changeTaskStatus(id)} >Done</button>
+                    <button onClick={() => props.deleteTask(id)}>X</button>
+                </p>    
+            </div>
+         );
+    } else{
+        const finishDateItem = new Date(finishDate).toDateString();
+        return (
+            <div>
+                <p>
+                    <strong>{text}</strong> <em><span>(till {date})</span></em>
+                    <span> - task completed: {finishDateItem}</span>
+                    <button onClick={() => props.deleteTask(id)}>X</button>
+                </p>    
+            </div>
+        )
+    }
+    
 }
  
 export default SingleTask ;
--- src/components/TaskList/TaskList.js
+++ src/components/TaskList/TaskList.js
@@ -2,19 +2,43 @@ import React from 'react';
 import SingleTask from './SingleTask/SingleTask';
 
 const TaskList = (props) => {
-    const tasks = props.tasks.map(task => <SingleTask 
+    // const tasks = props.tasks.map(task => <SingleTask 
+    //     key={task.id} 
+    //     task={task} 
+    //     deleteTask={props.deleteTask} 
+    //     changeTaskStatus ={props.changeTaskStatus}
+    // />);
+
+    const active = props.tasks.filter(task => task.active);
+    const completed = props.tasks.filter(task => !task.active);
+
+    const activeTasks = active.map(task => <SingleTask 
         key={task.id} 
         task={task} 
         deleteTask={props.deleteTask} 
         changeTaskStatus ={props.changeTaskStatus}
     />);
-    
+
+    const completedTasks = completed.map(task => <SingleTask 
+        key={task.id} 
+        task={task} 
+        deleteTask={props.deleteTask} 
+        changeTaskStatus ={props.changeTaskStatus}
+    />);
+
     return ( 
-        <div>
-            <h2>Tasks to do</h2>
-            {tasks}
-           
-        </div>
+        <React.Fragment>
+            <div>
+                <h2>Tasks to do ({activeTasks.length})</h2>
+                {activeTasks.length > 0 ? activeTasks : "All tasks completed."}
+            </div>
+
+            <hr />
+            <div>
+                <h2>Completed ({completedTasks.length})</h2>
+                {completedTasks}
+            </div>
+       </React.Fragment>
      );
 }
  
